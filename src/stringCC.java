aa73f127ff3673e6e8c028d3d16bfab1eb37a165
Witrman <32022781+Witrman@users.noreply.github.com>
2017-10-28 18:02:36 +0400

    wd

--- src/stringCC.java
+++ src/stringCC.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class stringCC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s =sc.nextLine().split(" ");
+        System.out.println(s.length);
+    }
 }
